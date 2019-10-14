f64612b2889efc6440ee50ede98dc87c609027ec
Jacob Garby <j4cobgarby@gmail.com>
2019-10-14 15:50:42 +0100

    reorganised code

--- makefile
+++ makefile
@@ -1,7 +1,7 @@
+NASM_TARGETS=src/main.asm
+
 all:
 	mkdir -p build
-	nasm -felf32 boot/boot.asm -o build/boot.o
-	i686-elf-gcc -c kernel/kernel.c -o build/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
-	i686-elf-gcc -T linker.ld -o iso/boot/octos.bin -ffreestanding -O2 -nostdlib build/boot.o build/kernel.o -lgcc
+	nasm -felf32 src/main.asm -o build/main.o
+	i686-elf-gcc -T linker.ld -o iso/boot/octos.bin -ffreestanding -O2 -nostdlib build/main.o -lgcc
 	grub-mkrescue -o octos.iso iso
-
---
+++ src/main.asm
@@ -0,0 +1,33 @@
+%include "src/multiboot.asm"
+%include "src/common.asm"
+
+section .bss
+align 16
+;; the stack is defined here.
+;; defined in .bss to save kernel space, since it'll just say
+;; "16384 uninitialised bytes here", rather than actually list
+;; that many bytes out in the object file!
+stack_bottom:
+    resb 16384
+stack_top:
+
+section .text   ; now the actual kernel entry point is in this section
+global _start:function (_start.end - _start)    ; make the object file store the length of the _start symbol
+
+%include "src/drivers/gdt.asm"
+%include "src/drivers/idt.asm"
+%include "src/drivers/pic.asm"
+%include "src/drivers/keyboard.asm"
+
+_start: ; kernel entry point
+        mov esp, stack_top      ; Set up stack
+        cli     ; Disable interrupts until everything is set up
+        lgdt [gdt_descriptor]
+        call refresh_segments
+        call init_keyboard      ; Put the address of the keyboard ISR
+                                ; into the IDT
+        lidt [idt_descriptor]
+        call pic_init           ; Set up the PIC
+.hltlp: hlt
+        jmp .hltlp
+.end:
