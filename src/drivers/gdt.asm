8716de59b21473abe8c6072d63c0190880467dd7
Jacob Garby <j4cobgarby@gmail.com>
2019-10-16 12:39:23 +0100

    beginning of userland GDT

--- src/drivers/gdt.asm
+++ src/drivers/gdt.asm
@@ -6,20 +6,27 @@
 gdt_start:
 gdt_null:
     dq 0 ; 8 bytes of nothing
-gdt_code:
+gdt_code:           ; == KERNEL CODE ==
     dw 0xffff       ; bits 0-15 of the length of the code segment
     dw 0x0000       ; bits 0-15 of the base of the code segment
     db 0x00         ; bits 16-23 of the base of the code segment
     db 0x9a         ; the access byte. this says that it can only be accessed by the kernel (ring 0)
     db 11001111b    ; bits 16-19 of the length of the segment, also flags saying 32 bit prot., and 4k blocks
     db 0x00         ; bits 24-31 of the base
-gdt_data:
+gdt_data:           ; == KERNEL DATA ==
     dw 0xffff       ; bits 0-15 of length
     dw 0x0000       ; bits 0-15 of base
     db 0x00         ; bits 16-23 of base
     db 0x92         ; access byte. non executable, only accessible by kernel (ring 0)
     db 11001111b    ; bits 16-19 of length, and also flags
     db 0x00         ; bits 24-31 of base
+gdt_userland_code:  ; == USER CODE ==
+    dw 0xffff       ; 0-15 of length
+    dw 0x0000       ; 0-15 of base
+    db 0x00         ; 16-23 of base
+    db 11111010b    ; this says it's ring 3, code, only accessed by ring 3
+    db 11001111b    ; 16-19 of length, also 32 bit and 4k granularity
+    db 0x00         ; 24-31 of base
 gdt_length equ $ - gdt_start - 1
 
 gdt_descriptor  dw gdt_length   ; the length of the gdt
