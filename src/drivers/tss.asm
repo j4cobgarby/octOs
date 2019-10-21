f2933edcad35fbf2558945a8b12fc92fcb4cc925
Jacob Garby <j4cobgarby@gmail.com>
2019-10-21 12:51:42 +0100

    procedure for loading data into tss

--- src/drivers/tss.asm
+++ src/drivers/tss.asm
@@ -1,6 +1,7 @@
 tss_entry:
     dd 0x00000000 ; prev_tss: unused
     dd 0x00000000 ; esp0: the stack pointer which needs to be loaded when kernel mode is entered
-    dd 0x00000002 ; ss0: the kernel stack segment (the data segment in the GDT)
+    dd 0x00000010 ; ss0: the kernel stack segment (the data segment in the GDT)
     dd 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0    ; i won't use the rest of the TSS
                                                         ; because i'm using software task switching
+tss_limit equ $ - tss_entry
