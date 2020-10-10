837661a38f532aa7abac50e77f1547359ae78774
Jacob Garby <psyjg12@nottingham.ac.uk>
2020-10-10 18:54:29 +0100

    added debug mode for messages

--- subprojects/kernel/src/arch/i386/pmm.c
+++ subprojects/kernel/src/arch/i386/pmm.c
@@ -11,25 +11,29 @@ void pmm_init(uint32_t* mboot_info, uint8_t* bitmap) {
     uint32_t entry_length;
     uint32_t entry_type;
 
+#ifdef KERNEL_DEBUG
     kio_puts("multiboot flags: ");
     kio_puthex(*mboot_info);
     kio_puts("\nKernel: ");
     kio_puthex((uint32_t)(&_kernel_start));
     kio_puts(" -> ");
     kio_puthex((uint32_t)(&_kernel_end));
-    kio_puts("\nInitialising pmm bitmap.\n");
+    kio_putc('\n');
+#endif
+    kio_puts("Initialising pmm bitmap...");
 
     if ((*mboot_info) & 0x1) { // mem_lower and mem_upper are valid
-        kio_puts("Reading memory size...\n");
         mem_lower = (*mboot_info + 4); // lower memory KB
         mem_upper = (*mboot_info + 8); // upper memory KB
+#ifdef KERNEL_DEBUG
         kio_puts("Lower memory limit (KBs): ");
         kio_puthex(mem_lower);
         kio_puts("\nUpper memory limit (KBs): ");
         kio_puthex(mem_upper);
         kio_putc('\n');
+#endif
     } else {
-        kio_puts("Multiboot header didn't provide required info.\n");
+        kio_puts("\nMultiboot header didn't provide required info.\n");
         while(1);
     }
 
@@ -43,45 +47,40 @@ void pmm_init(uint32_t* mboot_info, uint8_t* bitmap) {
         bitmap[block] = 1;
     }
 
+    kio_puts("Done.\n");
+
     if ((*mboot_info) & 0x40) {
-        kio_puts("Reading memory map...\n");
+        kio_puts("Reading memory map...");
 
         mmap_length = mboot_info[11];
         mmap_addr = mboot_info[12];
 
-        kio_puts("Mmap address: ");
+#ifdef KERNEL_DEBUG
+        kio_puts("\nMMap address: ");
         kio_puthex(mmap_addr);
-        kio_puts("   Mmap length: ");
+        kio_puts("   MMap length: ");
         kio_puthex(mmap_length);
-        kio_putc('\n');
-    /*
-        mmap_entry_start = (uint32_t*)mmap_addr;
-
-        while ((uint32_t)mmap_entry_start < mmap_addr + mmap_length) {
-            kio_puts("====== memory map entry ======\n");
-            entry_size = *(mmap_entry_start);
-            kio_puthex((uint32_t)mmap_entry_start);
-            kio_putc('\n');
-            kio_puthex(entry_size);
-            kio_putc('\n');
-            //entry_base = *(mmap_entry_start + 4);
-            //entry_length = *(mmap_entry_start + 12);
-            //entry_type = *(mmap_entry_start + 20);
-
-            //kio_puthex(entry_type);
-            mmap_entry_start += 0x18;
-        }*/
+        kio_puts("\n");
+#endif
 
         for (mmap_entry_start = (uint32_t*)mmap_addr
             ; (uint32_t)mmap_entry_start < mmap_addr + mmap_length
-            ; mmap_entry_start += (entry_size + sizeof(uint32_t))/4) {
-            kio_puts("====== memory map entry ======\n");
-            entry_size = *(mmap_entry_start);
-            kio_puthex((uint32_t)mmap_entry_start);
+            ; mmap_entry_start += (entry_size + 4)/4) {
+            entry_size      = mmap_entry_start[0];
+            entry_base      = mmap_entry_start[1];
+            entry_length    = mmap_entry_start[3];
+            entry_type      = mmap_entry_start[5];
+
+#ifdef KERNEL_DEBUG
+            kio_puts("- Memory region: ");
+            kio_puthex(entry_base);
+            kio_puts(" ==> ");
+            kio_puthex(entry_base + entry_length);
             kio_putc('\n');
-            kio_puthex(entry_size);
-            kio_putc('\n');
+#endif
         }
+
+        kio_puts("Done.");
     } else {
         kio_puts("Multiboot header didn't provide memory map.\n");
         while(1);
